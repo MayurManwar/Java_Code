0f4a502bb3764239731b9ecec79b26474961952c
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-13 19:16:44 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+package PracticeForInterview.copy;
+
+public class Factorial {
+public static void main(String[] args) {
+	
+
+	int n=5;
+int	fact=1;
+	
+	
+	while(n>0){
+		
+		fact=fact*n;
+		
+	n--;
+		
+	}
+	
+	System.out.println(fact);
+	
+}
+}
