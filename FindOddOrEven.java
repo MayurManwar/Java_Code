b58403f44c69cb02f047ed2db7a030885d57acaf
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-17 23:17:19 +0530

    Add files via upload

---
+++ FindOddOrEven.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+public class FindOddOrEven {
+
+	void bb(int a) {
+
+		if (a % 2 == 0) {
+
+			System.out.println("Given number is Even");
+
+		}
+
+		else {
+			System.out.println(" Given number is odd");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		FindOddOrEven nn = new FindOddOrEven();
+		nn.bb(554);
+
+	}
+
+}
