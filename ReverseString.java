88428833e17c479229bbb961cc96b4b3c65f9fff
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-17 23:27:50 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+package com.StringProgram;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+	String m="Mayur";
+	String rev="";
+	
+	for (int i=m.length ()-1; i>=0; i-- ) {
+		
+	      rev=rev+m.charAt(i);
+	      
+	   
+		
+	}
+		
+			System.out.print(rev);
+	}
+
+}
