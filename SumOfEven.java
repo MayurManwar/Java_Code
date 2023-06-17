5899f3c29101c52c362a06e2dc75fc9e035887c4
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-17 23:18:30 +0530

    Add files via upload

---
+++ SumOfEven.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+public class SumOfEven {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for (int i = 1; i <= 50; i++) {
+
+			if(i%2==0){
+				
+				sum=sum+i;
+				
+			}
+			
+	}
+		System.out.println(sum);
+
+}
+}
