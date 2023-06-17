ce5d00a59ac30410098266cb0b670db6d1fbe4dd
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-17 23:17:36 +0530

    Add files via upload

---
+++ FindOdd.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class FindOdd {
+
+	void m1(int m) {
+
+		if (m % 2 == 1) {
+
+			System.out.println(" Given Number is Odd");
+
+		} else {
+			System.out.println(" Given Number isn't  Valid");
+		}
+
+	}
+
+}
