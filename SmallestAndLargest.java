1b64aa8114cb8c4ead176444d6511e86b3d022bf
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2023-06-17 23:17:54 +0530

    Add files via upload

---
+++ SmallestAndLargest.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+public class SmallestAndLargest {
+
+	void m1(int a, int b) {
+
+		if (a < b) {
+
+			System.out.println(" A is Smallest Number..");
+
+		} else {
+
+			System.out.println(" A is largest Number...");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		SmallestAndLargest mm = new SmallestAndLargest();
+
+		mm.m1(546, 77);
+
+	}
+
+}
